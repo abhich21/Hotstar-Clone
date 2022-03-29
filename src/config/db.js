cb5c980f5761debf893892880ad149e3f920e21f
Vijendra <docs.vijendra@gmail.com>
2022-03-30 05:04:09 +0530

    database configration is dnoe completely.

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
-const db = "mongodb://localhost:27017/hotstar"
+
+
+// const db = "mongodb://localhost:27017/hotstar"
+// const KEY = 'hotstar'
+db = `mongodb+srv://hotstar:hotstar@cluster0.eavlw.mongodb.net/hotstar?retryWrites=true&w=majority`
 
 module.exports = () => mongoose.connect(db)
