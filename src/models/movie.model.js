f177dfb40d7cbb28b36abe72cc46753e1df754e5
Vijendra <docs.vijendra@gmail.com>
2022-03-30 06:23:54 +0530

    done models

--- src/models/movie.model.js
+++ src/models/movie.model.js
@@ -1,14 +1,16 @@
-//text
 const mongoose = require('mongoose')
 
-const movieShema = new mongoose.Schema({
+const movieShema = new mongoose.Schema(
+    {
     title : { type : String, required : true},
     image : { type : String, required : true},
     original_language : { type : String, required : true}
-},{
-    versionKey : false,
-    timestamps : true
-})
+    },
+    {
+        versionKey : false,
+        timestamps : true
+    }
+)
 
 module.exports = mongoose.connect('movie',movieShema)
 
