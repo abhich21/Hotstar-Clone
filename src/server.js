2c5f8698df802890a943e156773f2c2accf7dafa
Vijendra <docs.vijendra@gmail.com>
2022-03-30 06:24:33 +0530

    all routes are added in server.js file

--- src/server.js
+++ src/server.js
@@ -3,26 +3,16 @@
 const mongodbConnect = require('./config/db')
 
 
+
+const movieController = require('./controllers/movie.controller')
+app.use('/', movieController)
+
+
 const port = 7000 || PORT
-
-// app.use(express.json())
-app.get('/',async (req,res)=>{
-    try {
-        return res.send('HI ANONYMOUS!')
-    } catch (error) {
-        return res
-        .status(500)
-        .send({
-            message : error.message,
-            location : "server.js"
-        })
-    }
-})
-
 module.exports = ()=>{
     try {
         app.listen(port, async ()=>{
-            await mongodbConnect()
+            // await mongodbConnect()
             console.log(`Server is running on the port ${port}`)
         })
     } catch (error) {
