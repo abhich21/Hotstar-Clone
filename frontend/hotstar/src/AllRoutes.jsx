0c4b4d12d0c72edae8d04a783fa45fe531bcfc06
Vijendra <docs.vijendra@gmail.com>
2022-04-01 20:10:21 +0530

    changes for some testing  reasom.

--- frontend/hotstar/src/AllRoutes.jsx
+++ frontend/hotstar/src/AllRoutes.jsx
@@ -4,6 +4,7 @@ import IndividualPage from "./components/IndividualPage/IndividualPage";
 import MainPage from "./components/MainPage/MainPage";
 import Navbar from "./components/Navbar/Navbar";
 import VideoPage from "./components/videoPage/VideoPage";
+import { WatchList } from "./components/Watchlist/WatchList";
 
 function AllRoutes() {
   return (
@@ -14,7 +15,7 @@ function AllRoutes() {
         <Route path="/:category" element={<MainPage key={Date.now().toString}></MainPage>} ></Route>
         <Route  path={`/:category/:id`}  element={<IndividualPage key={Date.now().toString} ></IndividualPage>} ></Route>
         <Route path="/:category/:id/video" element={<VideoPage></VideoPage>}></Route>
-        
+        <Route path="/watchlist" element={<WatchList/>}></Route>
       </Routes>
       <Footer></Footer>
     </>
