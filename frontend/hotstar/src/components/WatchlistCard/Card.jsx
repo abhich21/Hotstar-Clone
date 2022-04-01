839845d4779addeaaf55259c9fec25029ad1c2e8
Vijendra <docs.vijendra@gmail.com>
2022-04-01 20:09:12 +0530

    New cards for wishlist are created.

---
+++ frontend/hotstar/src/components/WatchlistCard/Card.jsx
@@ -0,0 +1,32 @@
+import "./Card.css";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export const Card = ({ id, title, description, imageUrl }) => {
+
+
+  console.log({ title, description, imageUrl })
+  return (
+    <Link to={`/${"movie"}/${id}`}>
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+        className="row-card"
+      >
+        <div className="wrap">
+          <div className="card-gradient">
+            <div className="card-content">
+              <h3>{title}</h3>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+//discription : 'https://api.themoviedb.org/3/movie/852659?api_key=3e3f0a46d6f2abc8e557d06b3fc21a77&language=en-US'
