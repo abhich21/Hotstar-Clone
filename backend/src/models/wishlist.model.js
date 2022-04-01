7854ed951db5ffaaab17ebe8bed56ca74e3c5091
Vijendra <docs.vijendra@gmail.com>
2022-04-01 20:06:38 +0530

    user_id is added.

--- backend/src/models/wishlist.model.js
+++ backend/src/models/wishlist.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const wishListSchema = new mongoose.Schema(
     {
+        user_id : { type : String},
         imageUrl : { type : String, required : true},
         title : { type : String, required : true},
         overview : { type : String, required : true},
