93768a69505241bd6fa876fa2652c4d17460b075
Sahil Munjhal <95855396+SahilMunjhal@users.noreply.github.com>
2022-04-02 18:26:28 +0530

    wishlist added

--- frontend/hotstar/src/components/Banner/Banner.jsx
+++ frontend/hotstar/src/components/Banner/Banner.jsx
@@ -6,11 +6,45 @@ import ShareRoundedIcon from "@mui/icons-material/ShareRounded";
 import AddIcon from '@mui/icons-material/Add';
 import CheckIcon from '@mui/icons-material/Check';
 import axios from "axios";
+import {useEffect} from "react";
 import { useState } from "react";
 
-function Banner({Status,did,setStatus,title, year, genre, description, img, idm, mediaType }) {
-  const { id, category } = useParams();
-  console.log("checker");
+function Banner({original_title, title, year, genre, description, img, idm, mediaType }) {
+  const[Status,setStatus]=useState(false);
+  const[wishid,setWishid]=useState("");
+  const {id, category} = useParams();
+  const [ wishdata , setWishData ] = useState([])
+  console.log(wishid);
+
+  async function getWishlist(){
+    const userToken = localStorage.getItem('token')
+    const token = JSON.parse(userToken)
+    const a = await fetch('http://localhost:7000/watchlist',{
+        method : "GET",
+        headers : {
+          "content-type" : "application/json",
+          Authentication : `Bearer ${token}`
+        }
+      })
+      const b = await a.json()
+      setWishData(b);
+    }
+useEffect(()=>{ getWishlist()},[])
+
+useEffect(()=>{
+  for(let i=0;i<wishdata.length;i++){
+    if(original_title==wishdata[i].title){
+      console.log("coming");
+      setStatus(true);
+      setWishid(wishdata[i]._id)
+    }
+  }
+},[wishdata]);
+
+useEffect(()=>{
+  setStatus(false)
+},[id]);
+
 
   async function addWatchList(){
     const userToken = localStorage.getItem('token')
@@ -30,23 +64,29 @@ function Banner({Status,did,setStatus,title, year, genre, description, img, idm,
           "content-type" : "application/json",
           Authentication : `Bearer ${token}`
         }
-      }).then(()=>{
-        alert("added to wishlist")
-      })
+      });
+      
       const b = await a.json()
+      console.log("data from the post request",b)
+      setWishid(b._id)
     }
     else
       alert('Please SignIn to add this movie in your watchlist')
 
   }
 
-  async function deleteWatchList(){
+   async function deleteWatchList(){
+     setStatus(false);
+     console.log(Status,"working");
     {
-      const a = await fetch(`http://localhost:7000/watchlist/${did}`,{
+      console.log(`http://localhost:7000/watchlist/${wishid}`)
+      const a = await fetch(`http://localhost:7000/watchlist/${wishid}`,{
+      
         method : "DELETE",
       });
     }
   }
+
   return (
     <Link  to={ mediaType=="tv"? `/tv/${idm}`:`/movie/${idm || id}`}>
       <div className="banner-container">
@@ -72,7 +112,7 @@ function Banner({Status,did,setStatus,title, year, genre, description, img, idm,
               </Link>
               <div>
                 <div className="playlist-btn">
-                  <PlaylistAddRoundedIcon  onClick={addWatchList} fontSize="large"></PlaylistAddRoundedIcon>
+                  {Status?<CheckIcon className="checkIcon" onClick={deleteWatchList} fontSize="large"></CheckIcon>:<AddIcon onClick={addWatchList} fontSize="large"></AddIcon>}
                   watchlist
                 </div>
                 <div>
--- frontend/hotstar/src/components/IndividualPage/IndividualPage.jsx
+++ frontend/hotstar/src/components/IndividualPage/IndividualPage.jsx
@@ -6,27 +6,9 @@ import CardRows from "../CardRows/CardRows";
 import CheckIcon from '@mui/icons-material/Check';
 
 function IndividualPage(props) {
-  const[Status,setStatus]=useState(false);
-  const[wishid,setWishid]=useState("");
-  const {id, category} = useParams()
+  const {id, category} = useParams();
   useEffect(() => { getData() }, [id])
 
-  const [ wishdata , setWishData ] = useState([])
-
-    async function getWishlist(){
-            const userToken = localStorage.getItem('token')
-            const token = JSON.parse(userToken)
-            const a = await fetch('http://localhost:7000/watchlist',{
-                method : "GET",
-                headers : {
-                  "content-type" : "application/json",
-                  Authentication : `Bearer ${token}`
-                }
-              })
-              const b = await a.json()
-              setWishData(b)
-            }
-    useEffect(()=>{ getWishlist()},[])
   
   const [data, setData] = useState({})
   const getData = async () => {
@@ -35,25 +17,14 @@ function IndividualPage(props) {
     setData(a.data)
   }
 
-useEffect(()=>{
-  for(let i=0;i<wishdata.length;i++){
-    if(data.original_title==wishdata[i].title){
-      setStatus(true);
-      console.log(wishdata[i]._id);
-      setWishid(wishdata[i]._id)
-    }
-  }
-});
-
-console.log(wishdata);
-
   let baseImgUrl = 'https://image.tmdb.org/t/p/original'
-
+  
+  console.log(data.original_title);
   
   return (
     
     <div key={props.pageId}>
-      <Banner Status={Status} id={id} did={wishid} setStatus={setStatus} img={`${baseImgUrl}${data.backdrop_path}`  } title={data.original_title || data.name} description={data.overview}></Banner>
+      <Banner original_title={data.original_title} id={id}  img={`${baseImgUrl}${data.backdrop_path}`  } title={data.original_title || data.name} description={data.overview}></Banner>
       <CardRows language={data.original_language} row_title="More Like This"></CardRows>
     </div>
   )
