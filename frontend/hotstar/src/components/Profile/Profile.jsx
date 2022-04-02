4ba18dbb95b2ab283b0b51dfbd6a0add4d1ce6c5
Vijendra <docs.vijendra@gmail.com>
2022-04-02 14:37:22 +0530

    profile is added.

--- frontend/hotstar/src/components/Profile/Profile.jsx
+++ frontend/hotstar/src/components/Profile/Profile.jsx
@@ -1,9 +1,35 @@
+import "./profile.css"
 
-function Profile ( ){
-    
+export function Profile (){
+    let user = JSON.parse(localStorage.getItem('user'))
     return (
         <>
-            
+            <div className="containerdiv">
+                <div className="topdiv">
+                    <div>
+                        <h1>Profile</h1>
+                        <img className="user-img" src={user.picture ? user.picture : "https://via.placeholder.com/100x100"} alt="" />
+                        <h1 className="user-name">{user.name}</h1>
+                        <h2 className="user-mob">{user.email}</h2>
+                    </div>
+                </div>
+                <div className="bottomdiv">
+                    <div>
+                        Disney+ Hotstar Mobile for ₹ 499/year 
+                        Valid till: 27 Mar 2023</div>
+                    <div>UPGRADE PLAN</div>
+                    <div>View Transactions</div>
+                    <div>Account Settings</div>
+                    <div>Manage Devices</div>
+                    <div>Log Out All Devices</div>
+                </div>
+            </div>
         </>
     )
 }
+
+
+
+
+
+
