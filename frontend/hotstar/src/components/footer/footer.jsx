25843700785067e9233e8c9d5262f899d6530706
PrajwalDhoble <prsjwaldhoble2000@gmail.com>
2022-04-03 10:29:42 +0530

    ok

--- frontend/hotstar/src/components/footer/footer.jsx
+++ frontend/hotstar/src/components/footer/footer.jsx
@@ -33,7 +33,6 @@ function Footer() {
         <span><a href="https://play.google.com/store/apps/details?id=in.startv.hotstar"><img src="https://icon-library.com/images/download-on-the-app-store-icon/download-on-the-app-store-icon-5.jpg"></img></a></span><span><a href="https://apps.apple.com/in/app/hotstar/id934459219"><img src="https://icon-library.com/images/download-on-the-app-store-icon/download-on-the-app-store-icon-13.jpg"></img></a></span>
     </div>
     <div className="foot-mid1">
-      {/* <br></br> */}
         <div >Connect with us</div>
         <span><a href="https://facebook.com/DisneyPlusHotstar"><img src="https://cdn-icons-png.flaticon.com/128/1384/1384021.png"></img></a></span><span><a href="https://twitter.com/DisneyPlusHS"><img src="https://cdn-icons-png.flaticon.com/128/1384/1384049.png"></img></a></span>
         </div>
@@ -41,10 +40,6 @@ function Footer() {
         <div>Disney+Hotstar App</div>
         <span><a href="https://play.google.com/store/apps/details?id=in.startv.hotstar"><img src="https://icon-library.com/images/download-on-the-app-store-icon/download-on-the-app-store-icon-5.jpg"></img></a></span><span><a href="https://apps.apple.com/in/app/hotstar/id934459219"><img src="https://icon-library.com/images/download-on-the-app-store-icon/download-on-the-app-store-icon-13.jpg"></img></a></span>
     </div>
-     {/* <div className="foot-right1">
-        <div>Disney+Hotstar App</div>
-        <span><a href="https://play.google.com/store/apps/details?id=in.startv.hotstar"><img src="https://icon-library.com/images/download-on-the-app-store-icon/download-on-the-app-store-icon-5.jpg"></img></a></span><span><a href="https://apps.apple.com/in/app/hotstar/id934459219"><img src="https://icon-library.com/images/download-on-the-app-store-icon/download-on-the-app-store-icon-13.jpg"></img></a></span>
-    </div>  */}
     </div>
         <p>© 2022 STAR. All Rights Reserved. HBO, Home Box Office and all related channel and programming logos are service marks of, and all related programming </p>
         <p>visuals and elements are the property of, Home Box Office, Inc. All rights reserved.</p>
